434dad1aa4990988122abc5911acaa62f1e534b0
Subhajit Garai <128497014+Subhajitgarai@users.noreply.github.com>
2023-12-18 12:02:18 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,24 @@
+let input = document.getElementById('inputbox');
+let btn = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(btn);
+
+arr.forEach(button => {
+    button.addEventListener(function('click',e) {//When button is clicked
+        if (e.target.innerHTML === '=') {
+            string = eval(string);
+            input.value = string;
+        } else if (e.target.innerHTML === 'AEC') {
+            string = "";
+            input.value = string;
+        } else if(e.target.innerHTML=='DEL')
+        {
+            string=string.substring(0,string.length-1);
+            input.value=string;
+        } else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    });
+});
